2caa29d44e1cd91eed03525b266b5ef1e2469bbd
dawoodali09 <dawoodali@gmail.com>
2013-09-04 12:30:52 +0400

    DAL modified

---
+++ DAL/DBConnect.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Diagnostics;
+using System.Collections;
+using System.Data.SqlTypes;
+
+namespace PaymentGateWayAsia.DAL
+{
+    public enum DBConnections
+    {
+        PIS
+    }
+
+    public static class DBConnect
+    {
+        public static DataObject ExecuteReader(DBConnections objDBConnection, string StoredProcedure, Hashtable ht, bool IsInjectionSafe)
+        {
+            SqlDataReader dataresult = null;
+
+            try
+            {
+                SqlConnection Connection = new SqlConnection(GetConnectionString(objDBConnection));
+                SqlCommand Command = new SqlCommand();
+                Command.Connection = Connection;
+                Command.CommandType = CommandType.StoredProcedure;
+                Command.CommandText = StoredProcedure;
+
+                Command = AddParameters(Command, ht, IsInjectionSafe);
+
+                Connection.Open();
+
+                dataresult = Command.ExecuteReader(CommandBehavior.CloseConnection);
+
+                DataObject dataResult = new DataObject(dataresult);
+
+                return dataResult;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static DataObject ExecuteReader(string ConnectionString, string StoredProcedure, Hashtable ht, bool IsInjectionSafe)
+        {
+            SqlDataReader dataresult = null;
+
+            try
+            {
+                SqlConnection Connection = new SqlConnection(ConnectionString);
+                SqlCommand Command = new SqlCommand();
+                Command.Connection = Connection;
+                Command.CommandType = CommandType.StoredProcedure;
+                Command.CommandText = StoredProcedure;
+
+                Command = AddParameters(Command, ht, IsInjectionSafe);
+
+                Connection.Open();
+
+                dataresult = Command.ExecuteReader(CommandBehavior.CloseConnection);
+
+                DataObject dataResult = new DataObject(dataresult);
+
+                return dataResult;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static DataObject ExecuteReaderCommand(DBConnections objDBConnection, string StoredProcedure, Hashtable ht)
+        {
+            SqlDataReader dataresult = null;
+
+            try
+            {
+                SqlConnection Connection = new SqlConnection(GetConnectionString(objDBConnection));
+                SqlCommand Command = new SqlCommand();
+                Command.Connection = Connection;
+                Command.CommandType = CommandType.Text;
+                Command.CommandText = StoredProcedure;
+
+                Command = AddParameters(Command, ht, false);
+
+                Connection.Open();
+
+                dataresult = Command.ExecuteReader(CommandBehavior.CloseConnection);
+
+                DataObject dataResult = new DataObject(dataresult);
+
+                return dataResult;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static DataObject ExecuteReaderCommand(DBConnections objDBConnection, string CommandString)
+        {
+            SqlDataReader dataresult = null;
+
+            try
+            {
+                SqlConnection Connection = new SqlConnection(GetConnectionString(objDBConnection));
+                SqlCommand Command = new SqlCommand();
+                Command.Connection = Connection;
+                Command.CommandType = CommandType.Text;
+                Command.CommandText = CommandString;
+
+                Connection.Open();
+
+                dataresult = Command.ExecuteReader(CommandBehavior.CloseConnection);
+
+                DataObject dataResult = new DataObject(dataresult);
+
+                return dataResult;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public static DataObject ExecuteReaderCommand(string ConnectionString, string CommandString)
+        {
+            SqlDataReader dataresult = null;
+
+            try
+            {
+                SqlConnection Connection = new SqlConnection(ConnectionString);
+                SqlCommand Command = new SqlCommand();
+                Command.Connection = Connection;
+                Command.CommandType = CommandType.Text;
+                Command.CommandText = CommandString;
+
+                Connection.Open();
+
+                dataresult = Command.ExecuteReader(CommandBehavior.CloseConnection);
+
+                DataObject dataResult = new DataObject(dataresult);
+               // Connection.Close();
+                return dataResult;
+            }
+            catch (Exception e)
+            {
+              throw e;
+            }
+        }
+
+        public static void ExecuteNonQuery(string ConnectionString, string CommandString)
+        {
+            SqlDataReader dataresult = null;
+            try
+            {
+                SqlConnection Connection = new SqlConnection(ConnectionString);
+                SqlCommand Command = new SqlCommand();
+                Command.Connection = Connection;
+                Command.CommandType = CommandType.Text;
+                Command.CommandText = CommandString;
+
+                Connection.Open();
+
+                Command.ExecuteNonQuery();
+
+               // DataObject dataResult = new DataObject(dataresult);
+                 Connection.Close();
+               // return dataResult;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+
+        public static Object ExecuteScalar(DBConnections objDBConnection, string StoredProcedure, Hashtable ht, bool IsInjectionSafe)
+        {
+            SqlConnection Connection = new SqlConnection(GetConnectionString(objDBConnection));
+            SqlCommand Command = new SqlCommand();
+            Command.Connection = Connection;
+            Command.CommandType = CommandType.StoredProcedure;
+            Command.CommandText = StoredProcedure;
+
+            Command = AddParameters(Command, ht, IsInjectionSafe);
+
+            Connection.Open();
+            Object objObject = Command.ExecuteScalar();
+            Connection.Close();
+
+            return objObject;
+        }
+
+        public static void ExecuteNonQuery(DBConnections objDBConnection, string StoredProcedure, Hashtable ht, bool IsInjectionSafe)
+        {
+            SqlConnection Connection = new SqlConnection(GetConnectionString(objDBConnection));
+            SqlCommand Command = new SqlCommand();
+            Command.Connection = Connection;
+            Command.CommandType = CommandType.StoredProcedure;
+            Command.CommandText = StoredProcedure;
+
+            Command = AddParameters(Command, ht, IsInjectionSafe);
+
+            Connection.Open();
+
+            Command.ExecuteNonQuery();
+            Connection.Close();
+        }
+
+        public static void ExecuteNonQuery(string ConnectionString, string StoredProcedure, Hashtable ht, bool IsInjectionSafe)
+        {
+            SqlConnection Connection = new SqlConnection(ConnectionString);
+            SqlCommand Command = new SqlCommand();
+            Command.Connection = Connection;
+            Command.CommandType = CommandType.StoredProcedure;
+            Command.CommandText = StoredProcedure;
+
+            Command = AddParameters(Command, ht, IsInjectionSafe);
+
+            Connection.Open();
+
+            Command.ExecuteNonQuery();
+            Connection.Close();
+        }
+
+        #region "private methods"
+
+        private static SqlCommand AddParameters(SqlCommand objCommand, Hashtable ht, bool IsInjectionSafe)
+        {
+            if (ht != null)
+            {
+                if (IsInjectionSafe == false)
+                {
+                    foreach (string s in ht.Keys)
+                    {
+                        string input = ht[s].ToString();
+                        if (objCommand.CommandText == "Core_Errors_Add")
+                        {
+                            if (s.ToLowerInvariant() != "exception" && s.ToLowerInvariant() != "bloguserid" && s.ToLowerInvariant() != "created")
+                            {
+                                input = input.ToLowerInvariant().Replace("'", "&#39;").Replace("cast", "-CST-").Replace("exec", "-EKSEKVER-").Replace("execute", "-EKSEKVER-");
+                            }
+
+                            objCommand.Parameters.Add(new SqlParameter(s, input));
+                        }
+                        else
+                        {
+                            if (input.Contains("'") == true || input.ToLowerInvariant().Contains("cast") == true && input.ToLowerInvariant().Contains("exec") == true || input.ToLowerInvariant().Contains("execute") == true)
+                            {
+                                try
+                                {
+                                    objCommand.Parameters.Add(new SqlParameter(s, ht[s].ToString().Replace("'", "&#39;")));
+                                }
+                                catch (Exception) { }
+                            }
+                            else
+                            {
+                                objCommand.Parameters.Add(new SqlParameter(s, ht[s]));
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    foreach (string s in ht.Keys)
+                    {
+                        objCommand.Parameters.Add(new SqlParameter(s, ht[s]));
+                    }
+                }
+            }
+            return objCommand;
+        }
+
+        private static string GetConnectionString(DBConnections ConnectionString)
+        {
+            string ConnStr = "";
+            switch (ConnectionString)
+            {
+              
+                case DBConnections.PIS:
+                    ConnStr = "Data Source=cibdbuser.db.11702767.hostedresource.com;Initial Catalog=cibdbuser;Persist Security Info=True;User ID=cibdbuser; pwd=CIBdb123!;";             
+                   break;               
+                default:
+                    break;
+            }
+            return ConnStr;
+        }
+
+        #endregion
+    }
+}
---
+++ DAL/DataObject.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections;
+
+namespace PaymentGateWayAsia.DAL
+{
+    [Serializable]
+    public class DataObject
+    {
+        
+      
+        private ArrayList _fields = new ArrayList();
+        private List<DataObjectRow> _rows = new List<DataObjectRow>();
+        private bool firstrow = true;
+
+        public DataObject(IDataReader objDataReader)
+        {
+            DataTable dc = objDataReader.GetSchemaTable();
+
+            if (!objDataReader.IsClosed)
+            {
+                while (objDataReader.Read())
+                {
+                    object[] dataRow = new object[objDataReader.FieldCount];
+                    int i = 0;
+
+                    if (firstrow)
+                    {
+                        foreach (DataRow row in dc.Rows)
+                        {
+
+                            _fields.Add((string)row["columnname"]);
+                            i++;
+                        }
+
+                        firstrow = false;
+                    }
+
+                    _rows.Add(new DataObjectRow(ref _fields, objDataReader));
+                }
+
+                dc.Clear();
+                objDataReader.Close();
+                firstrow = true;
+            }
+            dc.Clear();
+            objDataReader.Close();
+        }
+
+        public List<DataObjectRow> Rows
+        {
+            get { return _rows; }
+        }
+    }
+
+    public class DataObjectRow
+    {
+        private ArrayList _values = new ArrayList();
+        private ArrayList _fields = null;
+
+        public DataObjectRow(ref ArrayList fields, IDataReader values)
+        {
+            _fields = fields;
+
+            int i = 0;
+            while (i < values.FieldCount)
+            {
+                _values.Add(values.GetValue(i));
+                i++;
+            }
+
+        }
+
+        public object Get(string Key)
+        {
+            string lowerKey = Key.ToLowerInvariant();
+            int counter = 0;
+            while (counter < _fields.Count)
+            {
+                if (_fields[counter].ToString().ToLowerInvariant() == lowerKey)
+                    return _values[counter];
+                counter++;
+            }
+            return null;
+        }
+
+        public Boolean HasValue(string Key)
+        {
+            string lowerKey = Key.ToLowerInvariant();
+            int counter = 0;
+            while (counter < _fields.Count)
+            {
+                if (_fields[counter].ToString().ToLowerInvariant() == lowerKey)
+                    if (_values[counter] != null)
+                        if (_values[counter] != DBNull.Value)
+                            return true;
+                counter++;
+            }
+            return false;
+        }
+
+        //public object GetValue(string Key, Delegate DataMember)
+        //{
+        //    string lowerKey = Key.ToLowerInvariant();
+        //    object objDataValue = null;
+        //    int cnt = 0;
+        //    while (cnt < _fields.Count && objDataValue == null)
+        //    {
+        //        if (((string)_fields[cnt]).ToLowerInvariant() == lowerKey)
+        //            if (_values[cnt] != null)
+        //                if (_values[cnt] != DBNull.Value)
+        //                    objDataValue = _values[cnt];
+        //        cnt++;
+        //    }
+
+        //    return Convert.ChangeType(objDataValue, DataMember.GetType());
+        //}
+
+    }
+}
