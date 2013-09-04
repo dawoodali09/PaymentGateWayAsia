5bd62e25a474b2bd3208300cc9a2870233468119
dawoodali09 <dawoodali@gmail.com>
2013-09-04 12:11:08 +0400

    werwe

--- www/Global.asax.cs
+++ www/Global.asax.cs
@@ -13,7 +13,7 @@ namespace www
         void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
-
+            //some comments added
         }
 
         void Application_End(object sender, EventArgs e)
